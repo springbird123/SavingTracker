13185bcb7e525239589dbbace03c3d9ebc7e0bbe
springbird123 <yubangkai1998@gmail.com>
2023-01-04 12:55:59 +1100

    creating menu

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
+import pandas as pd
+import os
+from art import *
 import menu
+clear = lambda: os.system('cls')
 
-#import clear
+def welcome():
+    logo = text2art("Saving Goal")
+    clear()
+    print(logo)
+    print("Welcome to the Saving Goal!")
+    print("")
 
 
-#clearing.clear()
+
+
+def main_menu():
+    welcome()
+    print("1. Creat a New Saving Goal")
+    print("2. Adding money to Saving Goal")
+    print("3. View and Edit Exsiting Saving Goals")
+    print("4. View Achieved Saving Goals")
+    print("5. Exit")
+    try:
+        ipt = int(input("Please enter the number:"))
+        if ipt == 1:
+            create_goal()
+        elif ipt == 2:
+            add_money()
+        elif ipt == 3:
+            view_goal()
+        elif ipt == 4:
+            view_achieved()
+        elif ipt == 5:
+            print("See you next time!")
+            exit()
+    except ValueError:
+        print("Please enter a valid number:")
+
+
 main_menu()
--- menu.py
+++ menu.py
@@ -1,9 +1,35 @@
+import pandas as pd
+import os
+import art
+#from wiper import wipe
+clear = lambda: os.system('cls')
+
+
 def main_menu():
+    print(text2art("Saving Goal"))
     print("1. Creat a New Saving Goal")
     print("2. Adding money to Saving Goal")
     print("3. View and Edit Exsiting Saving Goals")
-    print("4. View and Edit Achieved Saving Goals")
+    print("4. View Achieved Saving Goals")
     print("5. Exit")
-    input("Please enter the number:")
+    input = int(input("Please enter the number:"))
+    try:
+        if input == 1:
+            create_goal()
+        elif input == 2:
+            add_money()
+        elif input == 3:
+            view_goal()
+        elif input == 4:
+            view_achieved()
+        elif input == 5:
+            exit()
+    except ValueError:
+        print("Please enter a valid number")
 
-main_menu()
+
+
+
+
+
+
